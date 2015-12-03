65a6bd06e613a52a58971cf3cc49310d26ef498f
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2015-12-03 18:55:39 +0200

    johnny-five-test

---
+++ johnnytest.js
@@ -0,0 +1,15 @@
+var five = require("johnny-five");
+var Raspi = require("raspi-io");
+var board = new five.Board({
+  io: new Raspi()
+});
+
+board.on("ready", function() {
+	var pir = new five.Pin("P1-13");
+	
+
+	setInterval(	pin.read(function(error, value) {
+	  console.log(value);
+	}), 200);
+
+});
