c183c73dff339c6402dc0554b017959720bc7943
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2015-12-05 00:35:01 +0200

    lcdtest

---
+++ lcdtest.js
@@ -0,0 +1,20 @@
+var five = require("johnny-five");
+var Raspi = require("raspi-io");
+var board = new five.Board({
+  io: new Raspi()
+});
+
+board.on("ready", function() {
+	// Set LCD screen
+	var lcd = new five.LCD({
+		controller: "PCF8574AT",
+		address: 0x27
+	});
+
+	lcd.cursor(0,0).print("Hello World!")
+
+	// Function to print something on the lcd
+	function printToLcd(row, text) {
+		lcd.cursor(row, 0).print(text);
+	}
+});
