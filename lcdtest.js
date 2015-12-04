f16c2980bdd67dfdc2ed61696e5d8dba51c7756a
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2015-12-05 00:46:42 +0200

    lcdtest

--- lcdtest.js
+++ lcdtest.js
@@ -12,21 +12,6 @@ board.on("ready", function() {
 		address: 0x27
 	});
 
-// Tell the LCD you will use these characters:
-  lcd.useChar("check");
-  lcd.useChar("heart");
-  lcd.useChar("duck");
+	lcd.autoscroll().print("Bloop").print("Bleep").print("Bloop").print("Bleep").print("Bloop").print("Bleep").print("Bloop").print("Bleep").print("Bloop").print("Bleep").print("Bloop").print("Bleep").print("Bloop").print("Bleep").print("Bloop").print("Bleep");
 
-  // Line 1: Hi rmurphey & hgstrp!
-  lcd.clear().print("rmurphey, hgstrp");
-  lcd.cursor(1, 0);
-
-  // Line 2: I <3 johnny-five
-  // lcd.print("I").write(7).print(" johnny-five");
-  // can now be written as:
-  lcd.print("I :heart: johnny-five");
-
-  this.wait(3000, function() {
-    lcd.clear().cursor(0, 0).print("I :check::heart: 2 :duck: :)");
-  });
 });
