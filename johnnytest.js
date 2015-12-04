017306ce51af17af4e5ac1a365dcb4b4d7c2a427
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2015-12-05 00:34:18 +0200

    lcdtest

--- johnnytest.js
+++ johnnytest.js
@@ -3,22 +3,16 @@ var Raspi = require("raspi-io");
 var board = new five.Board({
   io: new Raspi()
 });
-
 board.on("ready", function() {
 	var pir = new five.Pin({
 		pin: "P1-13",
 		mode: 0
 	});
-
-	console.log(pir);
-
-
 	pir.on("high", function(e){
 		console.log("high", e);
-	})
+	});
 
 	pir.on("low", function(e){
 		console.log("low", e);
-	})
-
+	});
 });
