b07a889e1018697b8558bd72cfdcfa6d2524d896
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2015-12-05 00:43:43 +0200

    lcdtest

--- lcdtest.js
+++ lcdtest.js
@@ -1,5 +1,6 @@
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
+
 var board = new five.Board({
   io: new Raspi()
 });
@@ -11,6 +12,21 @@ board.on("ready", function() {
 		address: 0x27
 	});
 
-	lcd.cursor(0,0).print("HELLO WORLD!");
-	lcd.cursor(1,0).print("QRSTUVWXYZÅÖÄ");
+// Tell the LCD you will use these characters:
+  lcd.useChar("check");
+  lcd.useChar("heart");
+  lcd.useChar("duck");
+
+  // Line 1: Hi rmurphey & hgstrp!
+  lcd.clear().print("rmurphey, hgstrp");
+  lcd.cursor(1, 0);
+
+  // Line 2: I <3 johnny-five
+  // lcd.print("I").write(7).print(" johnny-five");
+  // can now be written as:
+  lcd.print("I :heart: johnny-five");
+
+  this.wait(3000, function() {
+    lcd.clear().cursor(0, 0).print("I :check::heart: 2 :duck: :)");
+  });
 });
