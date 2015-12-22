aa6deb3b1d332b997731cb5d8545bf0bc26d7466
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2015-12-22 20:09:52 +0200

    Robot 1

--- robot.js
+++ robot.js
@@ -11,30 +11,6 @@ var board = new five.Board({
 
 board.on("ready", function() {
 
-	console.log("Board ready");
-	// Set LCD screen
-	var lcd = new five.LCD({
-		controller: "PCF8574AT",
-		address: 0x27,
-		rows: 2,
-		cols: 16
-	});
-
-	lcd.clear().cursor(0, 0).print("BOARD READY");
-
-	// SOCKET.IO
-	var socket = io.connect('http://46.101.48.115:8080', {reconnect: true});
-
-	socket.on("disconnect", function(){
-	    console.log("DISCONNECTED");
-		lcd.clear().cursor(0,0).print("DISCONNECTED");
-	});
-
-	socket.on('connect', function() {
-		lcd.clear().cursor(0,0).print("CONNECTED");
-	    console.log('Connected!');
-	    socket.emit('camConnected');
-
 		// PIN OPERATIONS
 		var rightGearTick = new five.Pin({
 			pin: "GPIO4",
@@ -60,6 +36,32 @@ board.on("ready", function() {
 		leftGearTick.on("low", function(e){
 			console.log("right high");
 		});
+
+	console.log("Board ready");
+	// Set LCD screen
+	var lcd = new five.LCD({
+		controller: "PCF8574AT",
+		address: 0x27,
+		rows: 2,
+		cols: 16
+	});
+
+	lcd.clear().cursor(0, 0).print("BOARD READY");
+
+	// SOCKET.IO
+	var socket = io.connect('http://46.101.48.115:8080', {reconnect: true});
+
+	socket.on("disconnect", function(){
+	    console.log("DISCONNECTED");
+		lcd.clear().cursor(0,0).print("DISCONNECTED");
+	});
+
+	socket.on('connect', function() {
+		lcd.clear().cursor(0,0).print("CONNECTED");
+	    console.log('Connected!');
+	    socket.emit('camConnected');
+
+
 	});
 
 });
