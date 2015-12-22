8313b40908953264768fa374e7fb92c7d1f4eae4
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2015-12-22 20:07:08 +0200

    Robot 1

---
+++ robot.js
@@ -0,0 +1,68 @@
+console.log("Starting the system...");
+
+// WebSocket library
+var io = require('socket.io-client');
+
+var five = require("johnny-five");
+var Raspi = require("raspi-io");
+var board = new five.Board({
+  io: new Raspi()
+});
+
+board.on("ready", function() {
+
+	console.log("Board ready");
+	// Set LCD screen
+	var lcd = new five.LCD({
+		controller: "PCF8574AT",
+		address: 0x27,
+		rows: 2,
+		cols: 16
+	});
+
+	lcd.clear().cursor(0, 0).print("BOARD READY");
+
+	// SOCKET.IO
+	var socket = io.connect('http://46.101.48.115:8080', {reconnect: true});
+
+	socket.on("disconnect", function(){
+	    console.log("DISCONNECTED");
+		lcd.clear().cursor(0,0).print("DISCONNECTED");
+	});
+
+	socket.on('connect', function() {
+		lcd.clear().cursor(0,0).print("CONNECTED");
+	    console.log('Connected!');
+	    socket.emit('camConnected');
+
+		// PIN OPERATIONS
+		var rightGearTick = new five.Pin({
+			pin: "GPIO4",
+			mode: 0
+		});
+
+		// PIN OPERATIONS
+		var leftGearTick = new five.Pin({
+			pin: "GPIO17",
+			mode: 0
+		});
+
+		rightGearTick.on("high", function(e){
+			console.log("right high");
+		});
+
+		rightGearTick.on("low", function(e){
+			console.log("right high");
+		});
+
+		leftGearTick.on("high", function(e){
+			console.log("right high");
+		});
+
+		leftGearTick.on("low", function(e){
+			console.log("right high");
+		});
+	});
+
+});
+
