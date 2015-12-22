369c9747f3ae084cecaafc49f9de992cbd534f12
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2015-12-22 21:11:22 +0200

    Robot 1

--- robot.js
+++ robot.js
@@ -31,8 +31,8 @@ board.on("ready", function() {
 		}
 	});
 
-	leftMotor.start(60);
-	rightMotor.start(60);
+	leftMotor.forward(120);
+	rightMotor.forward(120);
 
 	///////////////////////
 	///		ENCODERS	///
@@ -57,7 +57,7 @@ board.on("ready", function() {
 
 	function rightTick(val) {
 		++rightTicks;
-		if (rightTicks > 25) {
+		if (rightTicks > 15) {
 			rightMotor.stop();
 			console.log("right stop");
 		}
@@ -66,7 +66,7 @@ board.on("ready", function() {
 
 	function leftTick(val) {
 		++leftTicks;
-		if (leftTicks > 25) {
+		if (leftTicks > 15) {
 			leftMotor.stop();
 			console.log("left stop");
 		}
