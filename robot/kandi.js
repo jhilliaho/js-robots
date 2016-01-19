c06089a37144a14fd57c61abf7857446649ec854
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-01-19 21:53:17 +0200

    Timing tests

--- robot/kandi.js
+++ robot/kandi.js
@@ -12,7 +12,7 @@ var camera = new RaspiCam({
 	output: "./images/image_%06d.jpg", // image_000001.jpg, image_000002.jpg,...
 	encoding: "png",
 	rotation: 180,
-	t: 1,
+	t: 2000,
 	n: true,
 	awb: false,
 	ev: 2,
