b07b6bd5d21fee096a3d161c420756bf79072b99
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-01-25 01:29:03 +0200

    Timing test 2

--- robot/timingtest.js
+++ robot/timingtest.js
@@ -1,8 +1,8 @@
 console.log("Starting timing test");
 
-setTimeout(function(){
+setInterval(function(){
 
-var time = Date.now();
-console.log(Date.now()-time);
+	var time = Date.now();
+	console.log(Date.now()-time);
 
 }, 200);
