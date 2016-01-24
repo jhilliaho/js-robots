77ecdd66b936130ff252a4da956c80efc44ae2e2
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-01-25 00:39:06 +0200

    Timing test 2

--- robot/kandi.js
+++ robot/kandi.js
@@ -25,8 +25,8 @@ var camera = new RaspiCam({
 	awb: false,								// No automatic white balance
 	shutter: 10000,						// Shutter time in microseconds
 	ISO: 200,								// ISO sensitivity
-	w: 640,									// Image width
-	h: 480									// Image height
+	w: 320,									// Image width
+	h: 240									// Image height
 
 });
 
