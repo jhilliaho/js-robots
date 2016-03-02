02964961737c2be5614f9601558f4c0ab0f8d88f
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-03 01:44:14 +0200

    serialtest

--- robot/serialtest.js
+++ robot/serialtest.js
@@ -1,40 +1,14 @@
-var five = require("johnny-five");
-var Raspi = require("raspi-io");
+var com = require("serialport");
 
-var board = new five.Board({
-  io: new Raspi()
+var serialPort = new com.SerialPort("/dev/ttyAMA0", {
+    baudrate: 9600,
+    parser: com.parsers.readline('\r\n')
+  });
+
+serialPort.on('open',function() {
+  console.log('Port open');
 });
 
-board.on("ready", function() {
-
-	console.log("Board ready");
-
-	var sp = require("serialport")
-
-	sp.list(function (err, ports) {
-	  ports.forEach(function(port) {
-	    console.log(port.comName);
-	  });
-	});
-
-
-	var SerialPort = sp.SerialPort;
-	var serialPort = new SerialPort("/dev/ttyAMA0", {
-	  baudrate: 9600
-	}); 
-
-
-	serialPort.on("open", function () {
-	  console.log('serial open');
-	  serialPort.on('data', function(data) {
-	    console.log('data received: ' + data);
-	  });
-	  serialPort.write("ls\n", function(err, results) {
-	    console.log('err ' + err);
-	    console.log('results ' + results);
-	  });
-	});
-
-
-
+serialPort.on('data', function(data) {
+  console.log(data);
 });
