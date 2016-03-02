ec0eb278a22336e2fab2aba1b12e6c369be96cbc
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-03 01:53:00 +0200

    serialtest

--- robot/serialtest.js
+++ robot/serialtest.js
@@ -1,14 +1,16 @@
 var com = require("serialport");
 
 var serialPort = new com.SerialPort("/dev/ttyAMA0", {
-    baudrate: 9600,
-    parser: com.parsers.readline('\r\n')
+    baudrate: 115200
   });
 
 serialPort.on('open',function() {
   console.log('Port open');
-	serialPort.on('data', function(data) {
-	  console.log(data);
-	});
 });
 
+
+serialPort.on('data', function(data) {
+  console.log(data);
+});
+
+while(1) {}
