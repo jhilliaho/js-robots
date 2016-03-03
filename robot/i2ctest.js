2a78cf1e4fc30ac837eee4a7d9be58cba6a521b1
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-03 21:15:22 +0200

    i2ctest

--- robot/i2ctest.js
+++ robot/i2ctest.js
@@ -28,3 +28,41 @@ board.on("ready", function() {
 		console.log("got ", string);
 	})
 });
+
+
+/*
+
+
+#include <Wire.h>
+
+void setup() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);
+  Serial.println("Starting");
+}
+
+void loop() {
+  delay(1000);
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  Wire.write("0000111122223333"); // respond with message of 16 bytes
+  // as expected by master
+}
+
+void receiveEvent(int howMany) {
+  String text = "";
+  while (0 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    text += String(c);
+  }
+  Serial.println(text);
+  Serial.println(text.indexOf("miten"));
+}
+
+
+ */
