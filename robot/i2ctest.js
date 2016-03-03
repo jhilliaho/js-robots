4e8a76cce80a701de1417a4c801d92b758dd487c
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-03 20:58:53 +0200

    i2ctest

--- robot/i2ctest.js
+++ robot/i2ctest.js
@@ -5,20 +5,16 @@ var board = new five.Board({
 });
 
 board.on("ready", function() {
-
 	var options = {
 		address: 1
 	};
 	board.io.i2cConfig(options);
 
-	board.io.i2cWrite(0x8, 0x41);
+	board.io.i2cWrite(0x8, 1,2,3,4,5,6,7,8);
 
 	board.io.i2cRead(0x8, 6, function(data){
 		var string = new Buffer(data).toString('ascii');
 
 		console.log("got ", string);
 	})
-
-
-
 });
