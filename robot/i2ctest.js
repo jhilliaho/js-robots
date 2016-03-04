312ecbf68dfebbe34f19b267da88333b03dbc14c
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-04 14:19:14 +0200

    i2ctest

--- robot/i2ctest.js
+++ robot/i2ctest.js
@@ -23,7 +23,7 @@ board.on("ready", function() {
 	var readNano = function readNano() {
 		board.io.i2cReadOnce(0x8, 3, function(data){
 
-			console.log(typeof data);
+			console.log(data, typeof data);
 			var difference = false;
 			for (var i = 0; i < data.length; ++i) {
 				if (lastData[i] != data[i]) {
