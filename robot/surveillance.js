1ed17cc748926a2723db1338699afff1aa7900ae
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-04 15:15:39 +0200

    firmata, surveillance

--- robot/surveillance.js
+++ robot/surveillance.js
@@ -16,44 +16,43 @@ board.on("ready", function() {
 
 	socket.on('connect', function() {
 	    console.log('Connected to server');
+
+		var options = {
+			address: 2
+		};
+
+		board.io.i2cConfig(options);
+
+		var lastData = [0,0,0]
+
+		var sendState = function sendState(data){
+			console.log("Sending data: ", data);
+			socket.emit("newData", data);
+		}
+
+		var sendPullUp = function sendPullUp(count) {
+			console.log("Sending pull-up!", count);
+			socket.emit("newPullUp", count);
+		}
+
+		var dataCounter = 0;
+
+		var readNano = function readNano() {
+			board.io.i2cReadOnce(0x8, 3, function(data){
+				dataCounter++;
+
+				var pullUps = data[data.length-1];
+				if (pullUps) {
+					sendPullUp(pullUps);
+				}
+
+				if (dataCounter >= 10) {
+					dataCounter = 0;
+					sendState(data);
+				}
+			})	
+		}
+
+		setInterval(readNano, 1000	);	
 	});
-
-	var options = {
-		address: 2
-	};
-
-	board.io.i2cConfig(options);
-
-	var lastData = [0,0,0]
-
-	var sendState = function sendState(data){
-		console.log("Sending data: ", data);
-		socket.emit("newData", data);
-	}
-
-	var sendPullUp = function sendPullUp(count) {
-		console.log("Sending pull-up!", count);
-		socket.emit("newPullUp", count);
-	}
-
-	var dataCounter = 0;
-
-	var readNano = function readNano() {
-		board.io.i2cReadOnce(0x8, 3, function(data){
-			dataCounter++;
-
-			var pullUps = data[data.length-1];
-			if (pullUps) {
-				sendPullUp(pullUps);
-			}
-
-			if (dataCounter >= 10) {
-				dataCounter = 0;
-				sendState(data);
-			}
-		})	
-	}
-
-	setInterval(readNano, 1000	);	
-
 });
