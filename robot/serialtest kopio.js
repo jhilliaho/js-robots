2923a22ddfa6999e3021663198d04b5d23d113eb
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-04 14:39:09 +0200

    surveillance

---
+++ robot/serialtest kopio.js
@@ -0,0 +1,16 @@
+var com = require("serialport");
+
+var serialPort = new com.SerialPort("/dev/ttyAMA0", {
+    baudrate: 9600,
+    parser: com.parsers.readline('\r\n')
+  });
+
+serialPort.on('open',function() {
+  console.log('Port open');
+});
+
+serialPort.on('data', function(data) {
+  console.log(data);
+});
+
+setInterval(function(){console.log("still working");}, 2000);
