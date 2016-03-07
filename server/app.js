4f3f8b3f3ff8b132ec45d1c9a2441e36382bb999
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-07 16:06:37 +0200

    surveillance

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require(express);
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 app.set('view engine', 'ejs');
