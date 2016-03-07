0e06c47683a9c2a758065735eca75a9c22efef66
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-07 14:04:20 +0200

    surveillance

--- server/app.js
+++ server/app.js
@@ -1,6 +1,3 @@
-console.log("It's now ", new Date());
-
-
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -15,39 +12,47 @@ var MongoClient = require('mongodb').MongoClient
  
 // Connection URL 
 var url = 'mongodb://localhost:27018/data';
+
 // Use connect method to connect to the Server 
 MongoClient.connect(url, function(err, db) {
   console.log("Connected correctly to server");
-
+  db = database;
+  // Insert some documents 
   db.collection('surveillanceData').find({}).toArray(function(err, docs) {
     console.log("Found the following records");
     console.dir(docs);
   });
 
-  //db.close();
-});
 
-io.on('connection', function (socket) {
-	console.log("connection");
-	socket.on('newImage', function (data) {
-		console.log("newImage");
-		require("fs").rename("images/out.jpg", ('images/' + Date.now() + '.jpg'), function(){
-			require("fs").writeFile("images/out.jpg", data, 'base64', function(err) {
-			  console.log("File written", err);
-			  socket.emit("imageReceived");
-			  imageDate = new Date();
+	io.on('connection', function (socket) {
+		console.log("connection");
+		socket.on('newImage', function (data) {
+			console.log("newImage");
+			require("fs").rename("images/out.jpg", ('images/' + Date.now() + '.jpg'), function(){
+				require("fs").writeFile("images/out.jpg", data, 'base64', function(err) {
+				  console.log("File written", err);
+				  socket.emit("imageReceived");
+				  imageDate = new Date();
+				});
 			});
 		});
+
+		socket.on('newPullUp', function (data) {
+			console.log("newPullUp ", data);
+		});
+
+		socket.on('newData', function (data) {
+			console.log("newData ", data);
+			var temperature = data[0];
+			var humidity = data[1];
+			var date = new Date();
+
+			db.collection('surveillanceData').insert({temperature: temperature, humidity: humidity, date: date}, function(err, result) {
+				console.log("Inserted", err, result);
+			});
+	  	});
 	});
 
-	socket.on('newPullUp', function (data) {
-		console.log("newPullUp ", data);
+	console.log("Started");
 
-	});
-
-	socket.on('newData', function (data) {
-		console.log("newData ", data);
-	});
 });
-
-console.log("Started");
