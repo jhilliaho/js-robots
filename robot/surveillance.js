4df02b85287a1f15cdaf85c9642a710170936176
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-17 17:43:20 +0200

    muutosta taas

--- robot/surveillance.js
+++ robot/surveillance.js
@@ -35,43 +35,42 @@ board.on("ready", function() {
 		var readNano = function readNano() {
 			//  byte arr[9] = {temp1, temp2, hum1, hum2, pirData, volumeDiff1, volumeDiff2, lightness1, lightness2};
 
-			try {
-				board.io.i2cReadOnce(0x8, 9, function(data){
-					console.log(dataCounter++);
+			board.io.i2cReadOnce(0x8, 9, function(data){
+				console.log(dataCounter++);
 
-					temperature = (data[0] + (data[1] << 8)) / 10;
-					humidity = (data[2] + (data[3] << 8)) / 10;
-					pir = data[4];
-					volume = data[5] + (data[6] << 8);
-					lightness = data[7] + (data[8] << 8);
+				temperature = (data[0] + (data[1] << 8)) / 10;
+				humidity = (data[2] + (data[3] << 8)) / 10;
+				pir = data[4];
+				volume = data[5] + (data[6] << 8);
+				lightness = data[7] + (data[8] << 8);
 
-					if (pir) {
-						motionDetected = true;
+				if (pir) {
+					motionDetected = true;
+				}
+
+				if (dataCounter >= 12) {
+					dataCounter = 0;
+					var pirVal;
+					if (motionDetected) {
+						pirVal = 1;
+					} else {
+						pirVal = 0;
 					}
-
-					if (dataCounter >= 12) {
-						dataCounter = 0;
-						var pirVal;
-						if (motionDetected) {
-							pirVal = 1;
-						} else {
-							pirVal = 0;
-						}
-						var newData = {temperature: temperature, humidity: humidity, pir: pirVal, volume: volume, lightness: lightness};
-						motionDetected = false;
-						console.log("Sending data: ", newData);
-						socket.emit("newData", newData);
-					}
-				});
-			} catch (err) {
-				console.log("ERROR IN I2C");
-			}
+					var newData = {temperature: temperature, humidity: humidity, pir: pirVal, volume: volume, lightness: lightness};
+					motionDetected = false;
+					console.log("Sending data: ", newData);
+					socket.emit("newData", newData);
+				}
+			});
 		}
 		setInterval(readNano, 500);	
 	});
 });
 
-
+process.on('uncaughtException', function(err) {
+    // handle the error safely
+    console.log("SOME ERROR HAPPENED", err);
+})
 
 
 
