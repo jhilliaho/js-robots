27612c3c6052b773e0d6332c7c9088fc1ef53079
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-29 19:47:55 +0300

    threeweedee

---
+++ robot/threeweedee.js
@@ -0,0 +1,35 @@
+var five = require("johnny-five");
+var Raspi = require("raspi-io");
+var board = new five.Board({
+  io: new Raspi()
+});
+
+board.on("ready", function() {
+
+  var compass = new five.Compass({
+    controller: "HMC5883L"
+  });
+
+  compass.on("change", function() {
+    console.log("change");
+    console.log("  heading : ", Math.floor(this.heading));
+    console.log("  bearing : ", this.bearing.name);
+    console.log("--------------------------------------");
+  });
+
+  compass.on("data", function() {
+    console.log("  heading : ", Math.floor(this.heading));
+    console.log("  bearing : ", this.bearing.name);
+    console.log("--------------------------------------");
+  });
+});
+
+process.on('uncaughtException', function(err) {
+    // handle the error safely
+    console.log("ERROR: ", err);
+})
+
+
+
+
+
