ce85010b2a398116515e9352d412680f23020545
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-29 22:37:34 +0300

    threeweedee

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -6,40 +6,57 @@ var board = new five.Board({
 
 board.on("ready", function() {
 
-	console.log("Ready");
 
-	var options = {
-		address: 2
-	};
+	console.log("connecting");
+	
+	var socket = require('socket.io-client')('http://46.101.79.118:3000');
 
-	board.io.i2cConfig(options);
-
-	var ENA = new five.Led({
-		address: 0x40,
-		controller: "PCA9685",
-		pin: 2,
+	socket.on("disconnect", function(){
+	    console.log("Disconnected from server");
 	});
 
-	var IN1 = new five.Led({
-		address: 0x40,
-		controller: "PCA9685",
-		pin: 1,
+	socket.on("newData", function(data){
+		console.log("Got new data, ", data);
 	});
 
-	var IN2 = new five.Led({
-		address: 0x40,
-		controller: "PCA9685",
-		pin: 0,
+	socket.once('connect', function() {
+	    console.log('Connected to server');
+
+
+		console.log("Ready");
+
+		var options = {
+			address: 2
+		};
+
+		board.io.i2cConfig(options);
+
+		var ENA = new five.Led({
+			address: 0x40,
+			controller: "PCA9685",
+			pin: 2,
+		});
+
+		var IN1 = new five.Led({
+			address: 0x40,
+			controller: "PCA9685",
+			pin: 1,
+		});
+
+		var IN2 = new five.Led({
+			address: 0x40,
+			controller: "PCA9685",
+			pin: 0,
+		});
+
+		ENA.brightness(255);
+		IN1.on();
+		IN2.off();
+
+		setTimeout(function(){
+			ENA.brightness(0);
+		}, 100);
 	});
-
-	ENA.brightness(255);
-	IN1.on();
-	IN2.off();
-
-	setTimeout(function(){
-		ENA.brightness(0);
-	}, 1000);
-
 
 
 });
