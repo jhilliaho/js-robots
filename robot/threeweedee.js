89e6237ee72b2ada6ae1edb91621bf805cc9983d
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-29 20:04:43 +0300

    threeweedee

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -14,23 +14,42 @@ board.on("ready", function() {
 
 	board.io.i2cConfig(options);
 
-
-
-    var temperature = new five.Thermometer({
-    controller: "BMP180",
-    freq: 250
+  var imu = new five.IMU({
+    controller: "MPU6050"
   });
 
-  temperature.on("change", function() {
+
+  imu.on("change", function() {
     console.log("temperature");
-    console.log("  celsius      : ", this.celsius);
-    console.log("  fahrenheit   : ", this.fahrenheit);
-    console.log("  kelvin       : ", this.kelvin);
+    console.log("  celsius      : ", this.temperature.celsius);
+    console.log("  fahrenheit   : ", this.temperature.fahrenheit);
+    console.log("  kelvin       : ", this.temperature.kelvin);
     console.log("--------------------------------------");
+
+    console.log("accelerometer");
+    console.log("  x            : ", this.accelerometer.x);
+    console.log("  y            : ", this.accelerometer.y);
+    console.log("  z            : ", this.accelerometer.z);
+    console.log("  pitch        : ", this.accelerometer.pitch);
+    console.log("  roll         : ", this.accelerometer.roll);
+    console.log("  acceleration : ", this.accelerometer.acceleration);
+    console.log("  inclination  : ", this.accelerometer.inclination);
+    console.log("  orientation  : ", this.accelerometer.orientation);
+    console.log("--------------------------------------");
+
+    console.log("gyro");
+    console.log("  x            : ", this.gyro.x);
+    console.log("  y            : ", this.gyro.y);
+    console.log("  z            : ", this.gyro.z);
+    console.log("  pitch        : ", this.gyro.pitch);
+    console.log("  roll         : ", this.gyro.roll);
+    console.log("  yaw          : ", this.gyro.yaw);
+    console.log("  rate         : ", this.gyro.rate);
+    console.log("  isCalibrated : ", this.gyro.isCalibrated);
+    console.log("--------------------------------------");
+    this.recalibrate();
   });
-
-
- });
+});
 
 process.on('uncaughtException', function(err) {
     // handle the error safely
