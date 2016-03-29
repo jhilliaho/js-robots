f4a74b49ef8bcc24f8c3be0884da3f2b8a3c481c
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-29 20:08:50 +0300

    threeweedee

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -20,7 +20,9 @@ board.on("ready", function() {
 
 
   imu.on("change", function() {
-    console.log("temperature");
+/*
+
+     console.log("temperature");
     console.log("  celsius      : ", this.temperature.celsius);
     console.log("  fahrenheit   : ", this.temperature.fahrenheit);
     console.log("  kelvin       : ", this.temperature.kelvin);
@@ -36,16 +38,10 @@ board.on("ready", function() {
     console.log("  inclination  : ", this.accelerometer.inclination);
     console.log("  orientation  : ", this.accelerometer.orientation);
     console.log("--------------------------------------");
-
-    console.log("gyro");
-    console.log("  x            : ", this.gyro.x);
-    console.log("  y            : ", this.gyro.y);
-    console.log("  z            : ", this.gyro.z);
+*/
     console.log("  pitch        : ", this.gyro.pitch);
     console.log("  roll         : ", this.gyro.roll);
     console.log("  yaw          : ", this.gyro.yaw);
-    console.log("  rate         : ", this.gyro.rate);
-    console.log("  isCalibrated : ", this.gyro.isCalibrated);
     console.log("--------------------------------------");
     this.gyro.recalibrate();
   });
