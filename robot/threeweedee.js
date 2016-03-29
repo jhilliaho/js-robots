614dcdcd5820fcf9144adec046699cb233bd85fc
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-29 19:58:22 +0300

    threeweedee

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -25,6 +25,19 @@ board.on("ready", function() {
     console.log("--------------------------------------");
   });
 
+    var temperature = new five.Thermometer({
+    controller: "BMP180",
+    freq: 250
+  });
+
+  temperature.on("change", function() {
+    console.log("temperature");
+    console.log("  celsius      : ", this.celsius);
+    console.log("  fahrenheit   : ", this.fahrenheit);
+    console.log("  kelvin       : ", this.kelvin);
+    console.log("--------------------------------------");
+  });
+
 
  });
 
