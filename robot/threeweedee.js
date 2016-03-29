eec3df1a5d79ac141acbd485c0ef5b6d9369e3cf
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-29 19:50:09 +0300

    threeweedee

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -14,22 +14,37 @@ board.on("ready", function() {
 
 	board.io.i2cConfig(options);
 
-  var compass = new five.Compass({
-    controller: "HMC5883L"
+  var imu = new five.IMU({
+    controller: "MPU6050"
   });
 
-  console.log("compass created");
-
-  compass.on("change", function() {
-    console.log("change");
-    console.log("  heading : ", Math.floor(this.heading));
-    console.log("  bearing : ", this.bearing.name);
+  imu.on("change", function() {
+    console.log("temperature");
+    console.log("  celsius      : ", this.temperature.celsius);
+    console.log("  fahrenheit   : ", this.temperature.fahrenheit);
+    console.log("  kelvin       : ", this.temperature.kelvin);
     console.log("--------------------------------------");
-  });
 
-  compass.on("data", function() {
-    console.log("  heading : ", Math.floor(this.heading));
-    console.log("  bearing : ", this.bearing.name);
+    console.log("accelerometer");
+    console.log("  x            : ", this.accelerometer.x);
+    console.log("  y            : ", this.accelerometer.y);
+    console.log("  z            : ", this.accelerometer.z);
+    console.log("  pitch        : ", this.accelerometer.pitch);
+    console.log("  roll         : ", this.accelerometer.roll);
+    console.log("  acceleration : ", this.accelerometer.acceleration);
+    console.log("  inclination  : ", this.accelerometer.inclination);
+    console.log("  orientation  : ", this.accelerometer.orientation);
+    console.log("--------------------------------------");
+
+    console.log("gyro");
+    console.log("  x            : ", this.gyro.x);
+    console.log("  y            : ", this.gyro.y);
+    console.log("  z            : ", this.gyro.z);
+    console.log("  pitch        : ", this.gyro.pitch);
+    console.log("  roll         : ", this.gyro.roll);
+    console.log("  yaw          : ", this.gyro.yaw);
+    console.log("  rate         : ", this.gyro.rate);
+    console.log("  isCalibrated : ", this.gyro.isCalibrated);
     console.log("--------------------------------------");
   });
 });
