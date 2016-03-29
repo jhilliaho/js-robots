471beb01c8ed66f37b3e02bb5a4e08565c77b0f6
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-29 22:34:42 +0300

    threeweedee

--- server/app.js
+++ server/app.js
@@ -10,100 +10,23 @@ server.listen(3000);
 
 app.use('/static', express.static(__dirname + '/public'));
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
- 
-// Connection URL 
-var url = 'mongodb://localhost:27018/data';
 
-// Use connect method to connect to the Server 
-MongoClient.connect(url, function(err, db) {
-  console.log("Connected correctly to server");
+io.on('connection', function (socket) {
+	console.log("connection");
 
-	io.on('connection', function (socket) {
-		console.log("connection");
-		socket.on('newImage', function (data) {
-			console.log("newImage");
-			require("fs").rename("images/out.jpg", ('images/' + Date.now() + '.jpg'), function(){
-				require("fs").writeFile("images/out.jpg", data, 'base64', function(err) {
-				  console.log("File written", err);
-				  socket.emit("imageReceived");
-				  imageDate = new Date();
-				});
-			});
-		});
-
-		socket.on('newData', function (data) {
-			console.log("newData ", data);
-			var temperature = data.temperature;
-			var humidity = data.humidity;
-			var pir = data.pir;
-			var lightness = 1023 - data.lightness;
-			var volume = data.volume;
-			var dataCounter = data.dataCounter;
-			var date = new Date();
-
-			db.collection('surveillanceData').insert({temperature: temperature, humidity: humidity, pir: pir, lightness: lightness, volume: volume, dataCounter: dataCounter, date: date}, function(err, result) {
-				console.log("Inserted", err, result);
-			});
-
-			socket.broadcast.emit("newData", {temperature: temperature, humidity: humidity, pir: pir, lightness: lightness, volume: volume, date: date});
-	  	});
-
-		var dataCountPerTime = 1000;
-		var getNthData = 100;
-
-		socket.on('getData', function () {
-			console.log("getData ");
-
-			db.collection('surveillanceData').count(function(err, dataCountInDb){
-
-				var getDataTimes = Math.ceil(dataCountInDb / dataCountPerTime);
-
-				for (var i = 0; i < getDataTimes; ++i) {
-					db.collection('surveillanceData').find({}).sort( { date: -1 } ).limit(dataCountPerTime).skip(dataCountPerTime*i).toArray(function(err, result) {
-						var data = [];
-						var motion = false;
-						var maxVolume = 0;
-						for (var i = 0; i < result.length; ++i) {
-							if (result[i].volume > maxVolume) {
-								maxVolume = result[i].volume;
-							}
-							
-							if (result[i].pir == 1) {
-								motion = true;
-							}
-							
-							if (i % getNthData == 0) {
-								if (motion) {
-									result[i].pir = 1;
-								}
-								result[i].volume = maxVolume;
-								data.push(result[i]);	
-								motion = false;
-								maxVolume = 0;
-							}
-						}
-						socket.emit("allData", data);
-					});				
-				}
-			});
-	  	});
-		socket.on("input1DataFromBrowser", function(data) {
-			console.log("Sending input1DataFromBrowser", data);
-			socket.broadcast.emit("input1DataFromBrowser", data);
-		})
-	});
-
-
-
-	app.get('/', function(req, res, next) {
-		res.render('index');
-	});
-
-	console.log("Started");
-	
+	socket.on('newData', function (data) {
+		socket.broadcast.emit("newData", data);
+  	});
 });
+
+
+
+app.get('/', function(req, res, next) {
+	res.render('gamepad');
+});
+
+console.log("Started");
+	
 
 
 
