5553511915990b004e132b555d1b7b6b128de038
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-03-29 20:21:00 +0300

    threeweedee

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -19,15 +19,37 @@ board.on("ready", function() {
   });
 
 
+  imu.on("change", function() {
+/*
+
+     console.log("temperature");
+    console.log("  celsius      : ", this.temperature.celsius);
+    console.log("  fahrenheit   : ", this.temperature.fahrenheit);
+    console.log("  kelvin       : ", this.temperature.kelvin);
+    console.log("--------------------------------------");
+
+    console.log("accelerometer");
+    console.log("  x            : ", this.accelerometer.x);
+    console.log("  y            : ", this.accelerometer.y);
+    console.log("  z            : ", this.accelerometer.z);
+    console.log("  pitch        : ", this.accelerometer.pitch);
+    console.log("  roll         : ", this.accelerometer.roll);
+    console.log("  acceleration : ", this.accelerometer.acceleration);
+    console.log("  inclination  : ", this.accelerometer.inclination);
+    console.log("  orientation  : ", this.accelerometer.orientation);
+    console.log("--------------------------------------");
+*/
+    console.log("  pitch        : ", this.gyro.pitch, "  roll         : ", this.gyro.roll, "  yaw          : ", this.gyro.yaw);
+  });
 
 imu.gyro.recalibrate();
 
 setInterval(function(){
-    console.log("  pitch        : ", imu.gyro.pitch);
-    console.log("  roll         : ", imu.gyro.roll);
-    console.log("  yaw          : ", imu.gyro.yaw);
+    console.log("  pitch        : ", this.gyro.pitch);
+    console.log("  roll         : ", this.gyro.roll);
+    console.log("  yaw          : ", this.gyro.yaw);
     console.log("--------------------------------------");
-}, 300);
+});
 
 });
 
