e5b2021fc22fd09053df278258d9eca9b2ca01e8
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-04-19 14:20:12 +0300

    testia

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -12,48 +12,50 @@ board.on("ready", function() {
 
 	board.io.i2cConfig(options);
 
-	var ENX = new five.Led({
+	var ENA = new five.Led({
 		address: 0x40,
 		controller: "PCA9685",
 		pin: 0,
 	});
 
-	var IN1X = new five.Led({
+	var ENB = new five.Led({
+		address: 0x40,
+		controller: "PCA9685",
+		pin: 1,
+	});
+
+	var IN1 = new five.Led({
 		address: 0x40,
 		controller: "PCA9685",
 		pin: 4,
 	});
 
-	var IN2X = new five.Led({
+	var IN2 = new five.Led({
 		address: 0x40,
 		controller: "PCA9685",
 		pin: 5,
 	});
 
-	var IN3X = new five.Led({
+	var IN3 = new five.Led({
 		address: 0x40,
 		controller: "PCA9685",
 		pin: 6,
 	});
 
-	var IN4X = new five.Led({
+	var IN4 = new five.Led({
 		address: 0x40,
 		controller: "PCA9685",
 		pin: 7,
 	});
 
-	ENX.off();
+	ENA.off();
+	ENB.off();
 	
 	IN1X.off();
 	IN2X.off();
 	IN3X.off();
 	IN4X.off();
 
-	for(var i = 0; i < 50; ++i) {
-		console.log(i);
-		IN1X.off();
-		IN1X.on();
-	}	
 
 
 
@@ -61,28 +63,9 @@ board.on("ready", function() {
 
 
 
-	console.log("connecting");
+
 	
 
-
-	var socket = require('socket.io-client')('http://46.101.79.118:3000');
-
-	socket.on("disconnect", function(){
-	    console.log("Disconnected from server");
-	});
-
-	socket.on("newData", function(data){
-		console.log("Got new data, ", data);
-
-	});
-
-	socket.once('connect', function() {
-	    console.log('Connected to server');
-
-
-		console.log("Ready");
-
-	});
 
 
 });
