8cdfc9f93bd1bab0939169eeb0442ad75ab50f1c
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-04-22 17:02:13 +0300

    i2ctest

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -41,6 +41,17 @@ board.on("ready", function() {
 	}
 
 	var calcMotorSpeeds = function calcMotorSpeeds() {
+
+		// Nollasuunta:
+		motor1.dir = 1;
+		motor2.dir = 0;
+		motor3.dir = 0;
+
+		motor1.speed = 100;
+		motor2.speed = 0;
+		motor3.speed = 100;		
+
+
 /*
 		motor1.dir = random(1,1);
 		motor2.dir = random(1,1);
