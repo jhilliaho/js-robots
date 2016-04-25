2059eb80243890c154b8523edfe97e0bb80c9b9d
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-04-25 15:14:01 +0300

    i2ctest

--- server/app.js
+++ server/app.js
@@ -44,10 +44,10 @@ io.on('connection', function (socket) {
 	socket.on('controllerDataFromBrowser', function (data) {
 		console.log("Sending controllerDataFromBrowser as speedAndAngleFromServer", data);
 		var data = {
-			angle1: posToAngle(x,y),
-			speed1: posToSpeed(x,y),
-			angle2: posToAngle(x,y),
-			speed2: posToSpeed(x,y)
+			angle1: posToAngle(data.x1,data.y1),
+			speed1: posToSpeed(data.x1,data.y1),
+			angle2: posToAngle(data.x2,data.y2),
+			speed2: posToSpeed(data.x2,data.y2)
 		}
 		socket.broadcast.emit("speedAndAngleFromServer", data);
 		console.log("angle and speed", posToAngle(data.x1, data.y1), posToSpeed(data.x1, data.y1));
