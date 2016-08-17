de35fb27b44986ec5edb638157145ac66253d0b1
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-08-17 12:36:50 +0300

    uusleiska

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -19,7 +19,7 @@ board.on("ready", function() {
 	var rollAngle = 0;
 
 	imu.on("change", function() {
-		rollAngle = this.gyro.roll.angle / 1.7;
+		rollAngle = this.gyro.roll.angle / 2;
 		console.log(rollAngle);
    	});
 
