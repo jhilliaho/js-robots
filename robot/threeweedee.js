2619ed9695d3b2f2d1ff4e5c74c2efb6dea19344
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-08-18 15:25:35 +0300

    uusleiska

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -197,6 +197,8 @@ board.on("ready", function() {
 			}
 		}
 		console.log("Widest angle: ", max, " at ", maxAngle);
+		maxAngle -= 270;
+		maxAngle = maxAngle < 0 ? maxAngle+360 : maxAngle;
 		return maxAngle;
 	}
 
