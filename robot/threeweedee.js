da03e7eb31bf01a94a766161a2bde13fb51e4a67
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-08-18 09:58:19 +0300

    uusleiska

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -64,16 +64,7 @@ board.on("ready", function() {
 		});		
 
 		compass.on("change", function() {
-			console.log("change");
 			console.log("  heading : ", Math.floor(this.heading));
-			console.log("  bearing : ", this.bearing.name);
-			console.log("--------------------------------------");
-		});
-
-		compass.on("data", function() {
-			console.log("  heading : ", Math.floor(this.heading));
-			console.log("  bearing : ", this.bearing.name);
-			console.log("--------------------------------------");
 		});
 	},200);
 
