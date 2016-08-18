cd1056a4987db489428b1ffd0c503412fb4ba7fc
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-08-18 15:00:25 +0300

    uusleiska

---
+++ robot/radar.js
@@ -0,0 +1,20 @@
+var five = require("johnny-five");
+var Raspi = require("raspi-io");
+var board = new five.Board({
+  io: new Raspi()
+});
+
+var SerialPort = require("serialport");
+
+board.on("ready", function() {
+
+	var range = 0;
+	var lidar = new SerialPort("/dev/ttyUSB0", {
+	  baudRate: 115200
+	});
+
+	lidar.on("data", function(data){
+		console.log(data);
+	});
+
+});
