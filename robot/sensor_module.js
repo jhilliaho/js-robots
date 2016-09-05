1d786fa50871037674e8eea32dddc2e139d9bf3f
Jani Hilliaho <jani.hilliaho@student.tut.fi>
2016-09-05 19:30:28 +0100

    coimbra

--- robot/sensor_module.js
+++ robot/sensor_module.js
@@ -5,21 +5,31 @@
 "use strict"
 
 var SerialPort = require("serialport");
-var five = require("johnny-five");
 
-var lidar = new SerialPort("/dev/ttyUSB0", {baudRate: 115200});
-var imu = new five.IMU({controller: "MPU6050"});
-var compass = new five.Compass({controller: "HMC5883L"});	
+exports.activateModule = activate;
 
-// Event handlers
-lidar.on('data', function (num) {
-	console.log("LidarData, ", num);
-});
+var board = {};
+var five = {};
 
-compass.on("change", function() {
-	console.log("CompassData: ", this.bearing.heading);
-});
+function activate(five_, board_){
+	five = five_;
+	board = board_;
+	var lidar = new SerialPort("/dev/ttyUSB0", {baudRate: 115200});
+	var imu = new five.IMU({controller: "MPU6050"});
+	var compass = new five.Compass({controller: "HMC5883L"});	
 
-imu.on("change", function() {
-	console.log("GyroData: ", this.gyro.roll.angle);
-});	
+	// Event handlers
+	lidar.on('data', function (num) {
+		console.log("LidarData, ", num);
+	});
+
+	compass.on("change", function() {
+		console.log("CompassData: ", this.bearing.heading);
+	});
+
+	imu.on("change", function() {
+		console.log("GyroData: ", this.gyro.roll.angle);
+	});		
+}
+
+
