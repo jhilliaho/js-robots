bdac84a5ebf1a4c3732e3859d5570c1d41af08b8
Jani Hilliaho <jani.hilliaho@gmail.com>
2016-09-06 20:38:44 +0100

    coimbra

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -4,8 +4,3 @@ var Raspi = require("raspi-io");
 var board = new five.Board({
   io: new Raspi()
 });
-
-// Own modules
-var moving = require("./moving_module.js");
-var connection = require("./connection_module.js");
-var sensors = require("./sensor_module.js");
