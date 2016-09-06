46ef1b15f009822b54e1ea6742311f37d5f24729
Jani Hilliaho <jani.hilliaho@gmail.com>
2016-09-06 20:49:51 +0100

    coimbra

---
+++ robot/programs_module.js
@@ -0,0 +1,44 @@
+// Node.js Module to calculate relative motor speeds by target angle for 3WD omniwheel robot
+//
+// Jani Hilliaho 2016
+
+	"use strict"
+
+	var moving = require("./moving_module.js");
+	var connection = require("./connection_module.js");
+	var sensors = require("./sensor_module.js");
+
+	exports.pointAngle = pointAngle;
+	exports.programLocks = programLocks;
+
+	var programLocks = {};
+
+	function pointAngle(destinationAngle) {
+		programLocks.pointAngleLock = false;
+		var interval = setInterval(function(){
+			if (programLocks.pointAngleLock) {return;}
+			programLocks.pointAngleLock = true;
+			var currentAngle = sensors.moduleState.compass;
+
+			if (Math.abs(currentAngle - destinationAngle) < 4) {
+				console.log("Now pointing to angle", destinationAngle);
+				clearInterval(interval);
+				moving.setMotorSpeeds(0,0,0);
+			} else {
+				var direction = 0;
+				if (Math.abs(destinationAngle - currentAngle) > 180) {
+					if (destinationAngle > currentAngle) {
+						destinationAngle -= 360;
+					} else {
+						currentAngle -= 360;
+					}
+				}
+				direction = (currentAngle < destinationAngle) ? 1 : -1;
+				var speed = Math.abs(destinationAngle - currentAngle) * 6;
+				speed = (speed > 60) ? 60 : speed;
+				console.log(currentAngle, " -> ", destinationAngle, " with direction ", direction, " and speed ", speed);
+				moving.setMotorSpeeds(0,0,speed * direction);
+			}
+			programLocks.pointAngleLock = false;
+		},20);
+	}	
