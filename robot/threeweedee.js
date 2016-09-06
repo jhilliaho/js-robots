792c1b3b248b9a571af95586f8b6f9d4d15488d5
Jani Hilliaho <jani.hilliaho@gmail.com>
2016-09-06 20:39:38 +0100

    coimbra

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -1,3 +1,69 @@
 // Node.js modules
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
+var board = new five.Board({
+  io: new Raspi()
+});
+
+// Own modules
+var moving = require("./moving_module.js");
+var connection = require("./connection_module.js");
+var sensors = require("./sensor_module.js");
+
+board.on("ready", function() {
+	board.io.i2cConfig();
+	sensors.activateModule(five, board);
+	moving.activateModule(board);
+	console.log(sensors.moduleState);
+});
+
+process.on('uncaughtException', function(err) {
+    console.log("ERROR: ", err);
+    return true;
+})
+
+var programLocks = {
+
+};
+
+function run() {
+	pointAngle(50);
+}
+
+function pointAngle(destinationAngle) {
+
+	programLocks.pointAngleLock = false;
+	var interval = setInterval(function(){
+
+		if (programLocks.pointAngleLock) {return;}
+		programLocks.pointAngleLock = true;
+		var currentAngle = sensors.moduleState.compass;
+
+		if (Math.abs(currentAngle - destinationAngle) < 4) {
+			console.log("Now pointing to angle", destinationAngle);
+			clearInterval(interval);
+			moving.setMotorSpeeds(0,0,0);
+		} else {
+			var direction = 0;
+
+			if (Math.abs(destinationAngle - currentAngle) > 180) {
+				if (destinationAngle > currentAngle) {
+					destinationAngle -= 360;
+				} else {
+					currentAngle -= 360;
+				}
+			}
+
+			direction = (currentAngle < destinationAngle) ? 1 : -1;
+
+			var speed = Math.abs(destinationAngle - currentAngle) * 6;
+			speed = (speed > 60) ? 60 : speed;
+
+			console.log(currentAngle, " -> ", destinationAngle, " with direction ", direction, " and speed ", speed);
+
+			moving.setMotorSpeeds(0,0,speed * direction);
+		}
+		programLocks.pointAngleLock = false;
+
+	},20);
+}
