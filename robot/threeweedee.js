07a215a84cdee7031299ac8c43db748e6f42c11b
Jani Hilliaho <jani.hilliaho@gmail.com>
2016-09-06 20:38:31 +0100

    coimbra

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -9,10 +9,3 @@ var board = new five.Board({
 var moving = require("./moving_module.js");
 var connection = require("./connection_module.js");
 var sensors = require("./sensor_module.js");
-
-board.on("ready", function() {
-	board.io.i2cConfig();
-	sensors.activateModule(five, board);
-	moving.activateModule(board);
-	console.log(sensors.moduleState);
-});
