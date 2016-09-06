6f6058951e86446877a5320c84c5d1ed7192df7a
Jani Hilliaho <jani.hilliaho@gmail.com>
2016-09-06 21:03:05 +0100

    coimbra

--- robot/moving_module.js
+++ robot/moving_module.js
@@ -41,7 +41,7 @@
 
 	function calcMotorSpeeds(angle, speed, rotation) {
 
-		angle += 60;
+		angle = 360 - angle;
 		console.log("CalcMotorSpeeds", angle, speed, rotation);
 
 		angle = parseInt(angle);
