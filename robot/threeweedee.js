0ec4fce0ae4a215e16d1834aedb01ab16d79142b
Jani Hilliaho <jani.hilliaho@gmail.com>
2016-09-06 20:49:30 +0100

    coimbra

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -7,6 +7,7 @@ var board = new five.Board({
 
 // Own modules
 var moving = require("./moving_module.js");
+var programs = require("./programs_module.js");
 var connection = require("./connection_module.js");
 var sensors = require("./sensor_module.js");
 
@@ -22,46 +23,6 @@ process.on('uncaughtException', function(err) {
     return true;
 })
 
-var programLocks = {};
-
 function run() {
-	pointAngle(50);
-}
-
-function pointAngle(destinationAngle) {
-
-	programLocks.pointAngleLock = false;
-	var interval = setInterval(function(){
-
-		if (programLocks.pointAngleLock) {return;}
-		programLocks.pointAngleLock = true;
-		var currentAngle = sensors.moduleState.compass;
-
-		if (Math.abs(currentAngle - destinationAngle) < 4) {
-			console.log("Now pointing to angle", destinationAngle);
-			clearInterval(interval);
-			moving.setMotorSpeeds(0,0,0);
-		} else {
-			var direction = 0;
-
-			if (Math.abs(destinationAngle - currentAngle) > 180) {
-				if (destinationAngle > currentAngle) {
-					destinationAngle -= 360;
-				} else {
-					currentAngle -= 360;
-				}
-			}
-
-			direction = (currentAngle < destinationAngle) ? 1 : -1;
-
-			var speed = Math.abs(destinationAngle - currentAngle) * 6;
-			speed = (speed > 60) ? 60 : speed;
-
-			console.log(currentAngle, " -> ", destinationAngle, " with direction ", direction, " and speed ", speed);
-
-			moving.setMotorSpeeds(0,0,speed * direction);
-		}
-		programLocks.pointAngleLock = false;
-
-	},20);
+	programs.pointAngle(50);
 }
