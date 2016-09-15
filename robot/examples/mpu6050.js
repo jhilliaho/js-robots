e52c900189a222107a0149ba07f4de0b78e47c48
Jani Hilliaho <jani.hilliaho@gmail.com>
2016-09-15 16:59:41 +0100

    keep angle

--- robot/examples/mpu6050.js
+++ robot/examples/mpu6050.js
@@ -14,16 +14,9 @@ board.on("ready", function() {
   });
 
   imu.on("change", function() {
-    console.log("gyro");
-    console.log("  x            : ", this.gyro.x);
-    console.log("  y            : ", this.gyro.y);
-    console.log("  z            : ", this.gyro.z);
-    console.log("  pitch        : ", this.gyro.pitch);
-    console.log("  roll         : ", this.gyro.roll);
+
     console.log("  yaw          : ", this.gyro.yaw);
-    console.log("  rate         : ", this.gyro.rate);
-    console.log("  isCalibrated : ", this.gyro.isCalibrated);
-    console.log("--------------------------------------");
+
   });
 
 });
