b730af7a3c8b2e652cec9ec9cd0163f4fd2bca2d
Jani Hilliaho <jani.hilliaho@gmail.com>
2016-09-17 17:49:46 +0100

    Radar data to web client

--- robot/connection_module.js
+++ robot/connection_module.js
@@ -30,7 +30,14 @@ socket.on("speedAndAngleFromServer", function(data){
 	exports.moduleState.lastDataPacket = data;
 });
 
+
+var lastSentRadarData = {angle: 0, distance: 0, date: 0};
 function sendRadarData(angle, distance) {
+	var dateNow = Date.now();
+	if (dateNow - lastSentRadarData.date < 500 && angle === lastSentRadarData.angle && Math.abs(lastSentRadarData.distance - distance) < 20) {
+		return;
+	}
+
 	console.log("Send Radar data", angle, distance);
 	socket.emit("radarData", {angle: angle, distance: distance});
 }
