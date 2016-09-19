9fe504d8d2bb64f5ddc4189a0503bdc4a1d7892b
Jani Hilliaho <jani.hilliaho@gmail.com>
2016-09-19 19:54:17 +0100

    Stop using remote server

--- robot/connection_module.js
+++ robot/connection_module.js
@@ -15,7 +15,10 @@ server.listen(3000);
 
 app.use('/static', express.static(__dirname + '/public'));
 
+var socket_
+
 io.on('connection', function (socket) {
+	socket_ = socket;
 	console.log("New connection");
 
 	socket.on('controllerDataFromBrowser', function (data) {
@@ -58,20 +61,6 @@ socket.on("speedAndAngleFromServer", function(data){
 	programs.newControllerData(data);
 });
 
-var lastSentRadarData = {angle: 0, distance: 0, date: 0};
-function sendRadarData(angle, distance) {
-	var dateNow = Date.now();
-	
-	if (dateNow - lastSentRadarData.date < 1000 && angle === lastSentRadarData.angle && Math.abs(lastSentRadarData.distance - distance) < 30) {
-		return;
-	}
-
-	lastSentRadarData.angle = angle;
-	lastSentRadarData.distance = distance;
-	lastSentRadarData.date = dateNow;
-
-	socket.emit("radarData", {angle: angle, distance: distance});
-}
 
 
 
@@ -95,4 +84,17 @@ exports.sendRadarData = sendRadarData;
 
 
 
+var lastSentRadarData = {angle: 0, distance: 0, date: 0};
+function sendRadarData(angle, distance) {
+	var dateNow = Date.now();
+	
+	if (dateNow - lastSentRadarData.date < 1000 && angle === lastSentRadarData.angle && Math.abs(lastSentRadarData.distance - distance) < 30) {
+		return;
+	}
 
+	lastSentRadarData.angle = angle;
+	lastSentRadarData.distance = distance;
+	lastSentRadarData.date = dateNow;
+
+	socket_.emit("radarData", {angle: angle, distance: distance});
+}
