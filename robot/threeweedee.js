d7b93a07f93f2640c2bb04e4ac08449b7b797c5b
Jani Hilliaho <jani.hilliaho@gmail.com>
2016-09-22 14:55:59 +0100

    Sbr

--- robot/threeweedee.js
+++ robot/threeweedee.js
@@ -1,11 +1,6 @@
 // Node.js modules
 // 11.7 = 64%
 // 11.46 = 41%
-// 
-// Luodaan robotista kauko-ohjattava siten, että sensoridata näkyy ohjatessa.
-// Piirretään tutkagraafi etäisyysmittarista ja osoitetaan robotin asento
-// 
-// 
 
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
